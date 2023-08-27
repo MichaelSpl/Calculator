a3b1e19ef8acf2fc7d8b546b5c45d552a0bfb2bd
MichaelSpl <splintermic@gmail.com>
2023-08-27 09:35:19 -0600

    bad changes

--- Calculator.js
+++ Calculator.js
@@ -2,50 +2,37 @@ function clearScreen() {
   document.getElementById("output").value = "";
 }
 
-function display(sym) {
-  document.getElementById("output").value += sym;
+function display(symbol) {
+  let symbol = document.getElementById("output").value;
+  document.getElementById("output").value += symbol;
 }
 
 function negOrPos() {
-  let exp = document.getElementById("output").value;
-  let newExp = "";
+  let symbol = document.getElementById("output").value;
+  let newSymbol = "";
 
-  for (let i = exp.length - 1; i >= 0; i--) {
-    if (isNaN(exp[i])) {
+  for (let i = symbol.length - 1; i >= 0; i--) {
+    if (isNaN(symbol[i])) {
       // Insert "(-" before arithmetic expression even if expression is -.
-      if(i-1 >= 0 && exp[i-1] == "(") {
-        newExp = exp.slice(0, i-1) + exp.slice(i+1);
-        document.getElementById("output").value = newExp;
+      if(i-1 >= 0 && symbol[i-1] == "(") {
+        newSymbol = symbol.slice(0, i-1) + symbol.slice(i+1);
+        document.getElementById("output").value = newSymbol;
       } else {
-        document.getElementById("output").value = exp + "(-";
+        alert(symbol[i]);
+        //newSymbol = symbol.slice(0, i) + "(-" + symbol.slice(i);
+        //document.getElementById("output").value = newSymbol;
+        document.getElementById("output").value = symbol + "(-";
       }
       return;
     }
   }
   
-  document.getElementById("output").value = "(-" + exp;
-  
-  /* Use for loop. Iterate through exp from end
-  to start until you stop either at start or before
-  an arithmetic symbol(+,-,*,÷). Insert "(-" at stop
-  location.*/
-
-  // if (exp.length-3 >= 0) {
-
-  // if (exp[exp.length-2] == "-" && exp[exp.length-3] == "(") { // splice doesn't work on string.
-  //newExp = exp.slice(0,exp.length-3) + exp.slice(exp.length-1);
-  //document.getElementById("output").value = newExp;
-  // } else {
-
-  // }
-  //} else {
-
-  //}
+  document.getElementById("output").value = "(-" + symbol;
 }
 
-function solveExp() {
-  let exp = document.getElementById("output").value;
+function solveProblem() {
+  let symbol = document.getElementById("output").value;
   let result = 0;
   let stack = [];
-  // alert(exp[exp.length-1]);
+  // alert(symbol[symbol.length-1]);
 }
