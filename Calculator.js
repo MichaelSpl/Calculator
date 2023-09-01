6bdcf52afb748712bb62ea136952378c21cd5f8f
MichaelSpl <splintermic@gmail.com>
2023-08-31 22:13:57 -0600

    Improved arithmetic symbol displaying

--- Calculator.js
+++ Calculator.js
@@ -3,7 +3,13 @@ function clearScreen() {
 }
 
 function display(sym) {
-  document.getElementById("output").value += sym;
+  let exp = document.getElementById("output").value;
+  let end = exp.substr(exp.length - 1);
+  if (isNaN(sym) && isNaN(end)) {
+    alert("Invalid Input");
+  } else {
+    document.getElementById("output").value += sym;
+  }
 }
 
 function negOrPos() {
@@ -13,8 +19,8 @@ function negOrPos() {
   for (let i = exp.length - 1; i >= 0; i--) {
     if (isNaN(exp[i])) {
       // Insert "(-" before arithmetic expression even if expression is -.
-      if(i-1 >= 0 && exp[i-1] == "(") {
-        newExp = exp.slice(0, i-1) + exp.slice(i+1);
+      if (i - 1 >= 0 && exp[i - 1] == "(") {
+        newExp = exp.slice(0, i - 1) + exp.slice(i + 1);
         document.getElementById("output").value = newExp;
       } else {
         document.getElementById("output").value = exp + "(-";
@@ -22,7 +28,7 @@ function negOrPos() {
       return;
     }
   }
-  
+
   document.getElementById("output").value = "(-" + exp;
 }
 
@@ -30,5 +36,4 @@ function solveExp() {
   let exp = document.getElementById("output").value;
   let result = 0;
   let stack = [];
-  // alert(exp[exp.length-1]);
 }
