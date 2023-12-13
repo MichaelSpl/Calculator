f34ec436317abbe15d893e455e1941e7f4c0f6b2
MichaelSpl <splintermic@gmail.com>
2023-12-13 11:11:42 -0600

    Removed -/+

--- Calculator.js
+++ Calculator.js
@@ -1,20 +1,4 @@
 function display(sym) {
-  /*let exp = document.getElementById("output").value;
-  let end = exp.substr(exp.length - 1);
-  if (isNaN(sym) && isNaN(end)) {
-    if (exp[exp.length - 2] == "(") {
-      if (sym == "÷" || sym == "X") {
-        end = exp.slice(0, exp.length - 1);
-        document.getElementById("output").value = end;
-        return;
-      }
-    }
-    // Problem here (click +/-, then X, then any other arithmetic symbol).
-    end = exp.slice(0, exp.length - 1) + sym;
-    document.getElementById("output").value = end;
-  } else {
-    document.getElementById("output").value += sym;
-  }*/
   document.getElementById("output").value += sym;
 }
 
@@ -46,26 +30,6 @@ function parenthesis() {
     document.getElementById("output").value += "(";
     parAmt++;
   }
-}
-
-function negOrPos() {
-  let exp = document.getElementById("output").value;
-  let newExp = "";
-
-  for (let i = exp.length - 1; i >= 0; i--) {
-    if (isNaN(exp[i])) {
-      // Insert "(-" before arithmetic expression even if expression is -.
-      if (i - 1 >= 0 && exp[i - 1] == "(") {
-        newExp = exp.slice(0, i - 1) + exp.slice(i + 1);
-        document.getElementById("output").value = newExp;
-      } else {
-        document.getElementById("output").value = exp + "(-";
-      }
-      return;
-    }
-  }
-
-  document.getElementById("output").value = "(-" + exp;
 }
 
 function solveExp() {
